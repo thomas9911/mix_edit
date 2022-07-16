be399622d8cf52356c27740abca914542da49311
Thomas <thomastimmer11@hotmail.com>
2022-07-16 15:09:40 +0200

    chore: add package and description

--- mix.exs
+++ mix.exs
@@ -8,12 +8,21 @@ defmodule MixEdit.MixProject do
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env())
+      elixirc_paths: elixirc_paths(Mix.env()),
+      description: "Elixir mix tasks to add, remove and update dependencies from mix.exs",
+      package: package()
     ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/thomas9911/mix_edit"}
+    ]
+  end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
